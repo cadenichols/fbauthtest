2bc83cbac84fda0c9ca71baf5b1a9767c3caecfe
cnichols <mellowdious@mail.com>
2015-06-17 13:32:17 -0700

    click handler

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 var ref = new Firebase('https://cadestest.firebaseio.com/');
 
+$(document).ready(init);
+
+function init(){
+  $('#login').on('click', loginClicked);
+}
+
+function loginClicked(){
+  
+  alert('sup');
+}
